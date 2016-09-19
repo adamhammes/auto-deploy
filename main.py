a48a27593a150ca5d8119cfd19682fadbb375b22
hammesa <hammesa@iastate.edu>
2016-09-19 18:39:04 -0400

    Implement functionality

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask
+import subprocess
+
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method != 'POST':
+        abort(501)
+
     data = request.get_json()
 
-    # do stuff
+    if data['ref'].endswith('/master'):
+        subprocess.call(['./build_site.sh'])
+
+    return '', 204
 
 if __name__ == '__main__':
-    app.run()
+    host = '0.0.0.0'
+    port = 9001
+    app.run(host=host, port=port)
